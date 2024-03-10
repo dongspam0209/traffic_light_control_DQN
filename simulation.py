03633c95e6e312e79d50aba8c2b64cbf5de9ab54
dongspam0209 <iankim010209@gmail.com>
2024-03-10 20:44:26 +0900

    existence

---
+++ simulation.py
@@ -0,0 +1,53 @@
+import traci
+import numpy as np
+import pandas as pd
+
+class Simulation:
+    def __init__(self,Cargenerator,sumo_cmd,max_steps,num_states):
+        self._Cargenerator=Cargenerator
+        self._sumo_cmd=sumo_cmd
+        self._max_steps=max_steps
+        self._num_states=num_states
+
+    def run(self,episode):
+        lane=["W_in_0","W_in_1","W_in_2","W_in_3","N_in_0","N_in_1","N_in_2","N_in_3",
+            "E_in_0","E_in_1","E_in_2","E_in_3","S_in_0","S_in_1","S_in_2","S_in_3"]
+        self._Cargenerator.generate_car(seed=episode)
+        traci.start(self._sumo_cmd)
+        print("Simulating")
+        for step in range(self._max_steps):
+            current_state=self._get_state()
+            df = pd.DataFrame(current_state,index=lane)
+            df.to_csv('./intersection/get_state.csv', index=True)
+            traci.simulationStep()
+
+        
+        traci.close()
+    
+    def _get_state(self):
+        lane=["W_in_0","W_in_1","W_in_2","W_in_3","N_in_0","N_in_1","N_in_2","N_in_3",
+              "E_in_0","E_in_1","E_in_2","E_in_3","S_in_0","S_in_1","S_in_2","S_in_3"]
+        state=np.zeros((16,100))
+        vehicle_list=traci.vehicle.getIDList()
+        lane_group=0
+
+        for veh_id in vehicle_list:
+            lane_position = traci.vehicle.getLanePosition(veh_id) # car position in lane
+            lane_id=traci.vehicle.getLaneID(veh_id) # -> lane
+            lane_position=700-lane_position # traffic light ~> max len of road 0~700
+            lane_cell= int(lane_position/7) # 7m : hegiht of the cell
+
+            for idx in range(len(lane)):
+                if lane_id==lane[idx]:
+                    lane_group=idx
+
+            state[lane_group][lane_cell]=1 # cell occupied
+
+        
+        
+        return state.tolist()
+
+
+
+
+        
--- training_main.py
+++ training_main.py
@@ -1,62 +1,42 @@
 from generator import CarGenerator
 from util import set_sumo
+from simulation import Simulation
 import matplotlib.pyplot as plt
 import traci
 
+################################################################
 total_episode=100
-
-
-def generate_car(seed):
-    max_steps = 3600  # max step in one episode
-    n_cars_generated = 1000  # number of car
-    car_generator = CarGenerator(max_steps, n_cars_generated)
-    car_generator.generate_car(seed)
-
-def get_queue_length():
-    halt_N=traci.edge.getLastStepHaltingNumber("N_in")
-    halt_S=traci.edge.getLastStepHaltingNumber("S_in")
-    halt_E=traci.edge.getLastStepHaltingNumber("E_in")
-    halt_W=traci.edge.getLastStepHaltingNumber("W_in")
-    total_queue_length=halt_N+halt_S+halt_E+halt_W
-
-    return total_queue_length
-
+max_steps=3600
+n_cars_generated=1000
+num_states=1600
+################################################################
 
 if __name__ == "__main__":
 
+#################################################################
     sumocfg_file_name = "cross.sumocfg"
     gui = True  # Change to False if you don't want the GUI
     max_steps=3600
     sumo_cmd = set_sumo(gui, sumocfg_file_name, max_steps)
-    queue_length_store=[]
+##################################################################
+    
+    CarGenerator=CarGenerator(
+        max_steps,
+        n_cars_generated
+    )
 
+    Simulation=Simulation(
+        CarGenerator,
+        sumo_cmd,
+        max_steps,
+        num_states
+    )
     episode=0
 
 
     while episode < total_episode:
         print(f'episode {episode}')
-        generate_car(episode)
-        
 
-        traci.start(sumo_cmd)
-    
-        for step in range(max_steps):
-            traci.simulationStep()  
-            total_queue_length=get_queue_length()
-        
-           
-        queue_length_store.append(total_queue_length)
-        traci.close()  
+        Simulation.run(episode)
 
         episode += 1
-
-    episode_numbers=range(1,episode+1)
-    plt.figure(figsize=(10,5))
-    plt.plot(episode_numbers,queue_length_store,linestyle='-',color='b')
-    plt.title('queue_length per episode')
-    plt.xlabel('episode number')
-    plt.ylabel('queue length')
-    plt.xticks(episode_numbers)
-    plt.grid(True)
-    plt.show()
-    # print(queue_length_store)
